2e65e3327689ea5fb875d60ae4f3623d8c0969cc
GilangRizkiPadilah1 <93955598+GilangRizkiPadilah1@users.noreply.github.com>
2021-12-10 21:32:02 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Darul 'Amal Lengkong | Selamat Datang</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="#">Darul 'Amal Lengkong</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Beranda <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="TK.html">Tk</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="SD.html">SD</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="MD.html">MD</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Kontak.html">Kontak</a>
+            </li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/Screenshot (167) (1).jpg" alt="Darul 'Amal" width="250" class="rounded-circle border border-dark">
+      <h1 class="display-5">Darul 'Amal Lengkong</h1>
+      <p class="lead">Darul 'Amal Berkarakter</p>
+    </section>
+    <!-- Akhir Jumbotron -->
+
+    <!-- Tentang -->
+    <section id="about">
+      <div class="container">
+        <div class="row text-center mt-3 mb-2">
+          <div class="col">
+            <h2>Sejarah Singkat</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col">
+            <p>Yayasan Pendidikan dan Sosial Islam (YAPSI) Darul 'Amal Jampang Kulon Cabang Lengkong merupakan yayasan berciri khas Islam. Darul 'Amal lahir dengan dukungan dari masyarakat sekitar, dibawah naungan Yayasan Pondok Pesantren Darul 'Amal Jampang Kulon. 
+              Pelopor pendiri Darul 'Amal Lengkong adalah Ustadz Affan Kusnadi, M. Pd. mendirikan yayasan ini pada tahun 2013. Beliau adalah salah satu anak angkat dan santri dari K.H. Umay (Pendiri Pondok Pesantren Darul 'Amal Jampang Kulon / pusat). 
+              Ustadz Affan  memilih tempat untuk mendirikan sekolah di tanah kelahiran beliau yaitu Kp. Tipar, Desa Tegallega, Kec. Lengkong, Kab. Sukabumi. Motivasi beliau mendirikan sekolah disini karena melihat anak-anak yang putus sekolah dikarenakan tidak ada biaya dan juga jarak ke sekolah yang jauh dan juga ingin memajukan pendidikan di desa tanah kelahiran. 
+              Pada awalnya beliau dan istrinya hanya mendirikan Taman Kanak-kanak (TK) saja, selang 2 tahun pada tahun 2015 karena dipercayai oleh warga desa untuk mendirikan Sekolah Dasar beliaupun mendirikan SD Darul 'Amal Lengkong. Yayasan ini murni dari swadaya masyarakat dan juga mencari donatur. 
+              Pada awal didirikannya TK hanyalah ruang madrasah kecil dan ruang SD pun masih menumpang dengan ruang TK, dengan sekolah secara bergantian. Alhamdulillah seiring berjalannya waktu dapat membangun bangunan sekolah dengan layak dan kokoh pada tahun 2017.  Ustadz Affan sebagai pendiri dan juga kepala dari YAPSI Darul 'Amal Lengkong hingga tahun 2020, sekarang telah digantikan oleh adik iparnya yaitu Bapak Panji Prawira, S. Ak.</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#b3c6e4" fill-opacity="1" d="M0,64L480,288L960,256L1440,64L1440,320L960,320L480,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir Tentang -->
+
+    <!-- Tentang Kami -->
+    <section id="Tentang Kami" style="background-color: #b3c6e4;">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Galeri Darul 'Amal</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/1.jpg" class="card-img-top" alt="Galeri 1">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/7.jpg" class="card-img-top" alt="Galeri 7">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/5.jpg" class="card-img-top" alt="Galeri 5">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/3.jpg" class="card-img-top" alt="Galeri 3">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/6.jpg" class="card-img-top" alt="Galeri 6">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/4.jpg" class="card-img-top" alt="Galeri 4">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/9.jpg" class="card-img-top" alt="Galeri 9">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-3 mb-3">
+            <div class="card">
+              <img src="img/Galeri/8.jpg" class="card-img-top" alt="Galeri 8">
+              <div class="card-body">
+                <p class="card-text"></p>
+              </div>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,320L480,96L960,128L1440,128L1440,320L960,320L480,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir Tentang Kami -->
+
+    <!-- Kontak -->
+    <section id="kontak">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Hubungi Kami</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">Nama Lengkap</label>
+                <input type="text" class="form-control" id="name" aria-describedby="name">
+                <div id="name" class="form-text">Wajib memasukan nama.</div>
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="email">
+              </div>
+              <div class="mb-3">
+                <label for="pesan" class="form-label">Pesan</label>
+                <textarea class="form-control" id="pesan" rows="3"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Kirim</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,96L480,224L960,128L1440,160L1440,320L960,320L480,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir Kontak -->
+
+    <!-- Footer -->
+    <Footer class="bg-primary text-white pb-3">
+      <p>Created By <a href="https://www.instagram.com/darulamallengkong/" class="text-white fw-bold">Darul 'Amal Lengkong</a></p>
+    </Footer>
+    <!-- Akhir Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
