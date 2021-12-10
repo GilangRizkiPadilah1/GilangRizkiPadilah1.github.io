30b55431d6d1ed5025508c8ff4b30b5ca853133b
GilangRizkiPadilah1 <93955598+GilangRizkiPadilah1@users.noreply.github.com>
2021-12-10 21:35:35 +0700

    Delete Footer.php

--- Footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Footer -->
-<Footer class="bg-primary text-white pb-3">
-      <p>Created By <a href="https://www.instagram.com/darulamallengkong/" class="text-white fw-bold">Darul 'Amal Lengkong</a></p>
-    </Footer>
-    <!-- Akhir Footer -->
